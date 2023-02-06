5aa04298545d6ac14b406759cea0724acd8a4d6e
Rohit Patel <90998294+rohu2405@users.noreply.github.com>
2023-02-06 22:03:23 +0530

    first commit

---
+++ app/src/main/java/com/example/foodrunner/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.foodrunner
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val startActivity = Intent(this@MainActivity , MainActivity2::class.java)
+            startActivity(startActivity)
+        }, 2000)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/foodrunner/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.foodrunner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
